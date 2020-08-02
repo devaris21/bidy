9bb6a7b9a0862a4b29b935b8e92259479deab057
Aristide Manyessé <aristidemanyesse@gmail.com>
2020-08-02 16:05:33 +0000

    mise a jour affichage caisse

--- webapp/gestion/modules/caisse/caisse/index.php
+++ webapp/gestion/modules/caisse/caisse/index.php
@@ -12,10 +12,158 @@
 
         <div id="page-wrapper" class="gray-bg">
 
-          <?php include($this->rootPath("webapp/gestion/elements/templates/header.php")); ?>  
+            <?php include($this->rootPath("webapp/gestion/elements/templates/header.php")); ?>  
 
 
-          <div class="wrapper wrapper-content">
+            <div class="wrapper wrapper-content">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="ibox ">
+                            <div class="ibox-title">
+                               <h5 class="float-left">Du <?= datecourt($date1) ?> au <?= datecourt($date2) ?></h5>
+                               <div class="float-right">
+                                <form id="formFiltrer" method="POST">
+                                    <div class="row" style="margin-top: -1%">
+                                        <div class="col-5">
+                                            <input type="date" value="<?= $date1 ?>" class="form-control input-sm" name="date1">
+                                        </div>
+                                        <div class="col-5">
+                                            <input type="date" value="<?= $date2 ?>" class="form-control input-sm" name="date2">
+                                        </div>
+                                        <div class="col-2">
+                                            <button type="button" onclick="filtrer()" class="btn btn-sm btn-white"><i class="fa fa-search"></i> Filtrer</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="ibox-content">
+                            <div class="row">
+                                <div class="col-lg-9">
+                                    <div class="flot-chart">
+                                        <div class="flot-chart-content" id="flot-dashboard-chart"></div>
+                                    </div><hr>
+                                    <div class="row" style="font-size: 10px">
+                                        <div class="col-sm">
+                                            <button data-toggle="modal" data-target="#modal-entree" class="btn btn-xs btn-primary dim"><i class="fa fa-check"></i> Nouvelle entrée</button>
+                                        </div>
+                                        <div class="col-sm text-center">
+                                            <button data-toggle="modal" data-target="#modal-depense" class="btn btn-xs btn-danger dim"><i class="fa fa-check"></i> Nouvelle dépense</button>
+                                        </div>
+                                        <div class="col-sm text-center">
+                                            <button data-toggle="modal" data-target="#modal-attente" class="btn btn-xs btn-success dim" ><i class="fa fa-eye"></i> Voir les versemments en attente</button>
+                                        </div>
+                                        <div class="col-sm">
+                                            <button data-toggle="modal" data-target="#modal-entree" class="btn btn-xs btn-primary dim pull-right"><i class="fa fa-check"></i> Transferts de caisse</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                    <ul class="stat-list">
+                                        <li>
+                                            <h2 class="no-margins">2,346</h2>
+                                            <small>Total reglements sur la periode</small>
+                                            <div class="stat-percent">48% <i class="fa fa-level-up text-navy"></i></div>
+                                            <div class="progress progress-mini">
+                                                <div style="width: 48%;" class="progress-bar"></div>
+                                            </div>
+                                        </li><br>
+                                        <li>
+                                            <h2 class="no-margins ">4,422</h2>
+                                            <small>Autres entrées</small>
+                                            <div class="stat-percent">60% <i class="fa fa-level-down text-navy"></i></div>
+                                            <div class="progress progress-mini">
+                                                <div style="width: 60%;" class="progress-bar"></div>
+                                            </div>
+                                        </li><br>
+                                        <li>
+                                            <h2 class="no-margins text-danger ">9,180</h2>
+                                            <small>Total des dépenses</small>
+                                            <div class="stat-percent">22% <i class="fa fa-bolt text-navy"></i></div>
+                                            <div class="progress progress-mini">
+                                                <div style="width: 22%;" class="progress-bar progress-bar-animated progress-bar-danger"></div>
+                                            </div>
+                                        </li><br>
+                                        <li>
+                                            <h2 class="no-margins text-warning">9,180</h2>
+                                            <small>Transferts de caisse</small>
+                                            <div class="stat-percent">22% <i class="fa fa-bolt text-navy"></i></div>
+                                            <div class="progress progress-mini">
+                                                <div style="width: 22%;" class="progress-bar progress-bar-animated progress-bar-warning"></div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="ibox">
+                        <div class="ibox-content">
+                            <div class="row">
+                                <div class="col-7">
+                                    <h5 class="text-uppercase">Chiffre d'affaire</h5>
+                                    <h1 class="no-margins">886,200</h1>
+                                </div>
+                                <div class="col-5 text-right">
+                                    <i class="fa fa-wrench fa-5x" style="color: #ddd"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="ibox">
+                        <div class="ibox-content">
+                            <div class="row">
+                                <div class="col-7">
+                                    <h5 class="text-uppercase">Dette Fournisseur</h5>
+                                    <h1 class="no-margins">886,200</h1>
+                                </div>
+                                <div class="col-5 text-right">
+                                    <i class="fa fa-eercast fa-5x" style="color: #ddd"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="ibox">
+                        <div class="ibox-content">
+                            <div class="row">
+                                <div class="col-7">
+                                    <h5 class="text-uppercase">Résultat brut</h5>
+                                    <h1 class="no-margins">886,200</h1>
+                                </div>
+                                <div class="col-5 text-right">
+                                    <i class="fa fa-cubes fa-5x" style="color: #ddd"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="ibox">
+                        <div class="ibox-content  bg-danger">
+                            <div class="row">
+                                <div class="col-7">
+                                    <h5 class="text-uppercase">Dette clientèle</h5>
+                                    <h1 class="no-margins">886,200</h1>
+                                </div>
+                                <div class="col-5 text-right">
+                                    <i class="fa fa-close fa-5x" style="color: #ddd"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
             <div class="row">
                 <div class="col-md-3">
                     <div class="ibox ">
@@ -25,7 +173,7 @@
                         </div>
                         <div class="ibox-content">
                             <h1 class="no-margins"><?= money(Home\COMMANDE::CA(date("Y")."-01-01" , dateAjoute())) ?></h1>
-                             <div class="stat-percent font-bold text-warning"><?= money(Home\CLIENT::dettes()) ?></div>
+                            <div class="stat-percent font-bold text-warning"><?= money(Home\CLIENT::dettes()) ?></div>
                             <small>Dette des clients</small>
                         </div>
                     </div>
@@ -349,6 +497,119 @@
 
         var ctx = document.getElementById("lineChart").getContext("2d");
         new Chart(ctx, {type: 'line', data: lineData, options:lineOptions});
+
+
+
+        var data2 = [
+        [gd(2012, 1, 1), 7], [gd(2012, 1, 2), 6], [gd(2012, 1, 3), 4], [gd(2012, 1, 4), 8],
+        [gd(2012, 1, 5), 9], [gd(2012, 1, 6), 7], [gd(2012, 1, 7), 5], [gd(2012, 1, 8), 4],
+        [gd(2012, 1, 9), 7], [gd(2012, 1, 10), 8], [gd(2012, 1, 11), 9], [gd(2012, 1, 12), 6],
+        [gd(2012, 1, 13), 4], [gd(2012, 1, 14), 5], [gd(2012, 1, 15), 11], [gd(2012, 1, 16), 8],
+        [gd(2012, 1, 17), 8], [gd(2012, 1, 18), 11], [gd(2012, 1, 19), 11], [gd(2012, 1, 20), 6],
+        [gd(2012, 1, 21), 6], [gd(2012, 1, 22), 8], [gd(2012, 1, 23), 11], [gd(2012, 1, 24), 13],
+        [gd(2012, 1, 25), 7], [gd(2012, 1, 26), 9], [gd(2012, 1, 27), 9], [gd(2012, 1, 28), 8],
+        [gd(2012, 1, 29), 5], [gd(2012, 1, 30), 8], [gd(2012, 1, 31), 25]
+        ];
+
+        var data3 = [
+        [gd(2012, 1, 1), 800], [gd(2012, 1, 2), 500], [gd(2012, 1, 3), 600], [gd(2012, 1, 4), 700],
+        [gd(2012, 1, 5), 500], [gd(2012, 1, 6), 456], [gd(2012, 1, 7), 800], [gd(2012, 1, 8), 589],
+        [gd(2012, 1, 9), 467], [gd(2012, 1, 10), 876], [gd(2012, 1, 11), 689], [gd(2012, 1, 12), 700],
+        [gd(2012, 1, 13), 500], [gd(2012, 1, 14), 600], [gd(2012, 1, 15), 700], [gd(2012, 1, 16), 786],
+        [gd(2012, 1, 17), 345], [gd(2012, 1, 18), 888], [gd(2012, 1, 19), 888], [gd(2012, 1, 20), 888],
+        [gd(2012, 1, 21), 987], [gd(2012, 1, 22), 444], [gd(2012, 1, 23), 999], [gd(2012, 1, 24), 567],
+        [gd(2012, 1, 25), 786], [gd(2012, 1, 26), 666], [gd(2012, 1, 27), 888], [gd(2012, 1, 28), 900],
+        [gd(2012, 1, 29), 178], [gd(2012, 1, 30), 555], [gd(2012, 1, 31), 993]
+        ];
+
+
+        var dataset = [
+        {
+            label: "Number of orders",
+            data: data3,
+            color: "#1ab394",
+            bars: {
+                show: true,
+                align: "center",
+                barWidth: 24 * 60 * 60 * 600,
+                lineWidth:0
+            }
+
+        }, {
+            label: "Payments",
+            data: data2,
+            yaxis: 2,
+            color: "#1C84C6",
+            lines: {
+                lineWidth:1,
+                show: true,
+                fill: true,
+                fillColor: {
+                    colors: [{
+                        opacity: 0.2
+                    }, {
+                        opacity: 0.4
+                    }]
+                }
+            },
+            splines: {
+                show: false,
+                tension: 0.6,
+                lineWidth: 1,
+                fill: 0.1
+            },
+        }
+        ];
+
+
+        var options = {
+            xaxis: {
+                mode: "time",
+                tickSize: [3, "day"],
+                tickLength: 0,
+                axisLabel: "Date",
+                axisLabelUseCanvas: true,
+                axisLabelFontSizePixels: 12,
+                axisLabelFontFamily: 'Arial',
+                axisLabelPadding: 10,
+                color: "#d5d5d5"
+            },
+            yaxes: [{
+                position: "left",
+                max: 1070,
+                color: "#d5d5d5",
+                axisLabelUseCanvas: true,
+                axisLabelFontSizePixels: 12,
+                axisLabelFontFamily: 'Arial',
+                axisLabelPadding: 3
+            }, {
+                position: "right",
+                clolor: "#d5d5d5",
+                axisLabelUseCanvas: true,
+                axisLabelFontSizePixels: 12,
+                axisLabelFontFamily: ' Arial',
+                axisLabelPadding: 67
+            }
+            ],
+            legend: {
+                noColumns: 1,
+                labelBoxBorderColor: "#000000",
+                position: "nw"
+            },
+            grid: {
+                hoverable: false,
+                borderWidth: 0
+            }
+        };
+
+        function gd(year, month, day) {
+            return new Date(year, month - 1, day).getTime();
+        }
+
+        var previousPoint = null, previousLabel = null;
+
+        $.plot($("#flot-dashboard-chart"), dataset, options);
+
     });
 </script>
 
